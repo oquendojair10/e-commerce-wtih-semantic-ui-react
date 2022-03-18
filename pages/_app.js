bbaea125b2c541411fb46d388f40ef255d7aa3c9
Jair Pérez <oquendojair10@gmail.com>
2022-03-18 13:10:56 -0500

    initial page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
+import CartProvider from "../context/cart.context";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,51 +1,81 @@
-import { Card, Image, Icon } from "semantic-ui-react";
+import { useContext } from "react";
+import useSWR from "swr";
+import fetcher from "../lib/fetcher";
+import { CartContext } from "../context/cart.context";
+
+import Container from "../components/Container";
+import { Card, Image, Icon, Header, Label, Button } from "semantic-ui-react";
+import { formatter } from "../lib/formatter";
+
 function HomePage() {
-	return (
-		<div className="container">
-			<Card.Group itemsPerRow={5}>
-				<Card>
-					<Image
-						src="https://react.semantic-ui.com/images/avatar/large/daniel.jpg"
-						wrapped
-						ui={false}
-					/>
-					<Card.Content>
-						<Card.Header>Daniel</Card.Header>
-						<Card.Meta>Joined in 2016</Card.Meta>
-						<Card.Description>
-							Daniel is a comedian living in Nashville.
-						</Card.Description>
-					</Card.Content>
-					<Card.Content>
-						<a>
-							<Icon name="user" />
-							10 Friends
-						</a>
-					</Card.Content>
-				</Card>
-				<Card>
-					<Image
-						src="https://react.semantic-ui.com/images/avatar/large/daniel.jpg"
-						wrapped
-						ui={false}
-					/>
-					<Card.Content>
-						<Card.Header>Daniel</Card.Header>
-						<Card.Meta>Joined in 2016</Card.Meta>
-						<Card.Description>
-							Daniel is a comedian living in Nashville.
-						</Card.Description>
-					</Card.Content>
-					<Card.Content>
-						<a>
-							<Icon name="user" />
-							10 Friends
-						</a>
-					</Card.Content>
-				</Card>
-			</Card.Group>
-		</div>
-	);
+  const { data, error } = useSWR("/api/bicycles", fetcher);
+  const { onAdd } = useContext(CartContext);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <Container>
+      <div className="home-wrapper">
+        <Card.Group itemsPerRow={3}>
+          {data.length ? (
+            data.map((item) => (
+              <Card key={item.bicycleId}>
+                <Image
+                  src={item.imageBicycle}
+                  style={{ width: "100%", height: 225, objectFit: "cover" }}
+                  fluid
+                />
+                <Card.Content>
+                  <Card.Header>{item.model}</Card.Header>
+                  {/* <Card.Meta>{item.date}</Card.Meta> */}
+                  {/* <Card.Description>{item.description}</Card.Description> */}
+                </Card.Content>
+                <Card.Content>
+                  {item.soldOut ? (
+                    <Label basic size="medium" color="red" ribbon>
+                      Sold Out
+                    </Label>
+                  ) : (
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Button
+                        size="mini"
+                        icon="shopping cart"
+                        onClick={() => onAdd({ ...item, quantity: 1 })}
+                        content="Add to cart"
+                        color="teal"
+                      />
+                      <span>{formatter.format(parseInt(item.price))}</span>
+                    </div>
+                  )}
+                </Card.Content>
+              </Card>
+            ))
+          ) : (
+            <Header
+              as="h4"
+              icon="meh outline"
+              content="No bicycles"
+              color="grey"
+              style={{ marginTop: "1em" }}
+            />
+          )}
+        </Card.Group>
+      </div>
+      <style jsx>{`
+        .home-wrapper {
+          margin: auto;
+          max-width: 800px;
+        }
+      `}</style>
+    </Container>
+  );
 }
 
 export default HomePage;
